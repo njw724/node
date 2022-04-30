5bdd3c032d545feb9f7ba9bf5ca68bee3f75e987
Jiwon Nam <njw724@naver.com>
2022-04-30 18:14:08 +0900

    Initial Commit

---
+++ 5-module/app.js
@@ -0,0 +1,8 @@
+// const counter = require('./counter.js')
+// import {increase, getCount} from './counter.js';
+import * as counter from './counter.js';
+
+console.log(increase())
+console.log(increase())
+console.log(increase())
+console.log(getCount());
---
+++ 6-os/app.js
@@ -0,0 +1,7 @@
+const os = require('os');
+
+console.log(os.EOL);
+
+
+console.log(os.EOL === '\n');
+console.log(os.EOL === '\r\n');
